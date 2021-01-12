76a0ce0431deb2969d0fb27312f01614cbde6a4d
rtroost <r.troost@quintor.nl>
2021-01-12 13:02:18 +0100

    controller arithmetic methods. still one failing test related to custom exception

--- src/main/java/nl/aegon/calculator/web/rest/CalculatorController.java
+++ src/main/java/nl/aegon/calculator/web/rest/CalculatorController.java
@@ -1,6 +1,7 @@
 package nl.aegon.calculator.web.rest;
 
 import nl.aegon.calculator.enums.CalculationType;
+import nl.aegon.calculator.exception.CalculatorException;
 import nl.aegon.calculator.service.CalculatorService;
 import nl.aegon.calculator.web.dto.CalculationDto;
 import org.springframework.http.ResponseEntity;
@@ -25,21 +26,28 @@ public class CalculatorController {
     public ResponseEntity<CalculationDto> add(@RequestBody CalculationDto input) {
         input.setType(CalculationType.ADDITION);
         input.setResult(calculatorService.add(input.getA(), input.getB()));
-        return ResponseEntity.ok(input);
+        return ResponseEntity.ok(input); // int primitives can be considered 'safe'
     }
 
     @PostMapping("/subtract")
     public ResponseEntity<CalculationDto> subtract(@RequestBody CalculationDto input) {
-        return null;
+        input.setType(CalculationType.SUBTRACTION);
+        input.setResult(calculatorService.subtract(input.getA(), input.getB()));
+        return ResponseEntity.ok(input);
     }
 
     @PostMapping("/divide")
-    public ResponseEntity<CalculationDto> divide(@RequestBody CalculationDto input) {
-        return null;
+    public ResponseEntity<CalculationDto> divide(@RequestBody CalculationDto input) throws CalculatorException {
+        System.out.println("Dividing " + input.getA() + " by " + input.getB());
+        input.setType(CalculationType.DIVISION);
+        input.setResult(calculatorService.divide(input.getA(), input.getB()));
+        return ResponseEntity.ok(input);
     }
 
     @PostMapping("/multiply")
     public ResponseEntity<CalculationDto> multiply(@RequestBody CalculationDto input) {
-        return null;
+        input.setType(CalculationType.MULTIPLICATION);
+        input.setResult(calculatorService.multiply(input.getA(), input.getB()));
+        return ResponseEntity.ok(input);
     }
 }
--- src/test/java/nl/aegon/calculator/web/rest/CalculatorControllerTest.java
+++ src/test/java/nl/aegon/calculator/web/rest/CalculatorControllerTest.java
@@ -2,6 +2,7 @@ package nl.aegon.calculator.web.rest;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import nl.aegon.calculator.enums.CalculationType;
+import nl.aegon.calculator.exception.CalculatorException;
 import nl.aegon.calculator.service.CalculatorService;
 import nl.aegon.calculator.web.dto.CalculationDto;
 import org.junit.jupiter.api.Test;
@@ -16,6 +17,7 @@ import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.times;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
@@ -47,7 +49,7 @@ public class CalculatorControllerTest {
                         .contentType(MediaType.APPLICATION_JSON_VALUE)
                         .accept(MediaType.APPLICATION_JSON_VALUE)
                         .content(objectMapper.writeValueAsString(input)))
-                .andExpect(result -> {
+                .andExpect(result -> { // then
                     final MockHttpServletResponse response = result.getResponse();
                     final CalculationDto output = objectMapper.readValue(response.getContentAsString(), CalculationDto.class);
                     assertEquals(HttpStatus.OK.value(), response.getStatus());
@@ -60,4 +62,111 @@ public class CalculatorControllerTest {
         Mockito.verify(calculatorServiceMock, times(1)).add(a, b);
     }
 
+    @Test
+    public void testCanPerformSubtraction() throws Exception {
+        // given
+        final CalculationDto input = new CalculationDto();
+        final int a = 10;
+        final int b = 10;
+        input.setA(a);
+        input.setB(b);
+
+        // when
+        Mockito.when(calculatorServiceMock.subtract(a, b)).thenReturn(0.0);
+
+        mockMvc.perform(post("/subtract")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .content(objectMapper.writeValueAsString(input)))
+                .andExpect(result -> { // then
+                    final MockHttpServletResponse response = result.getResponse();
+                    final CalculationDto output = objectMapper.readValue(response.getContentAsString(), CalculationDto.class);
+                    assertEquals(HttpStatus.OK.value(), response.getStatus());
+                    assertEquals(CalculationType.SUBTRACTION, output.getType());
+                    assertEquals(0.0, output.getResult());
+                    assertEquals(a, output.getA());
+                    assertEquals(b, output.getB());
+                });
+
+        Mockito.verify(calculatorServiceMock, times(1)).subtract(a, b);
+    }
+
+    @Test
+    public void testCanPerformDivision() throws Exception {
+        // given
+        final CalculationDto input = new CalculationDto();
+        final int a = 10;
+        final int b = 5;
+        input.setA(a);
+        input.setB(b);
+
+        // when
+        Mockito.when(calculatorServiceMock.divide(a, b)).thenReturn(2.0);
+
+        mockMvc.perform(post("/divide")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .content(objectMapper.writeValueAsString(input)))
+                .andExpect(result -> { // then
+                    final MockHttpServletResponse response = result.getResponse();
+                    final CalculationDto output = objectMapper.readValue(response.getContentAsString(), CalculationDto.class);
+                    assertEquals(HttpStatus.OK.value(), response.getStatus());
+                    assertEquals(CalculationType.DIVISION, output.getType());
+                    assertEquals(2.0, output.getResult());
+                    assertEquals(a, output.getA());
+                    assertEquals(b, output.getB());
+                });
+
+        Mockito.verify(calculatorServiceMock, times(1)).divide(a, b);
+    }
+
+    @Test
+    public void testCannotPerformDivisionByZero() throws Exception {
+        // given
+        final CalculationDto input = new CalculationDto();
+        final int a = 15;
+        final int b = 0;
+        input.setA(a);
+        input.setB(b);
+
+        // when
+        Mockito.when(calculatorServiceMock.divide(a, b)).thenThrow(CalculatorException.class);
+
+        mockMvc.perform(post("/divide")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .content(objectMapper.writeValueAsString(input)))
+                .andExpect(result ->
+                    assertTrue(result.getResolvedException() instanceof CalculatorException)
+                );
+    }
+
+    @Test
+    public void testCanPerformMultiplication() throws Exception {
+        // given
+        final CalculationDto input = new CalculationDto();
+        final int a = 10;
+        final int b = 10;
+        input.setA(a);
+        input.setB(b);
+
+        // when
+        Mockito.when(calculatorServiceMock.multiply(a, b)).thenReturn(100.0);
+
+        mockMvc.perform(post("/multiply")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .content(objectMapper.writeValueAsString(input)))
+                .andExpect(result -> { // then
+                    final MockHttpServletResponse response = result.getResponse();
+                    final CalculationDto output = objectMapper.readValue(response.getContentAsString(), CalculationDto.class);
+                    assertEquals(HttpStatus.OK.value(), response.getStatus());
+                    assertEquals(CalculationType.MULTIPLICATION, output.getType());
+                    assertEquals(100.0, output.getResult());
+                    assertEquals(a, output.getA());
+                    assertEquals(b, output.getB());
+                });
+
+        Mockito.verify(calculatorServiceMock, times(1)).multiply(a, b);
+    }
 }
